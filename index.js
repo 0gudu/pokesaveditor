387ce5784e6cd27c40b5823241d17241cffd59c7
gustavojp <gustavojp@wetzel.com.br>
2025-08-15 11:57:50 -0300

    name size validation

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const funcsv = require("./save/funcs");
 
 function getTrainerName(data) {
   const datafile = fs.readFileSync(data);
+
+  //location and size of the trainer name in the game memory
   var offset = 0x2598;
   var size = 8;
+  
   return funcsv.getTextString(offset, size, datafile);
 }
 
 function setTrainerName(name, arquivo){
   const datafile = fs.readFileSync(arquivo);
+
+  //location and size of the trainer name in the game memory
   var offset = 0x2598;
   var size = 8;
-  let data = funcsv.setTextString(offset, size, datafile, name );
 
+  if(size < name.length) return false;
+  let data = funcsv.setTextString(offset, size, datafile, name );
   fs.writeFileSync(arquivo, data);
   return true;
 }
